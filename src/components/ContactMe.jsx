758be9a5dd23ee9bf4b90985c1042e4e68ae6a5c
Martin Kirk <kirkovic@gmail.com>
2025-12-16 14:04:22 +0100

    pg 4

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Subtitle from "./ui/Subtitle";
 import Title from "./ui/Title";
 import Container from "./Container";
-import { contact1 } from "../assets/portfolioImages/contact1.png";
+//import { contact1 } from "../assets/portfolioImages/contact1.png";
 
 import SocialIcon from "./ui/SocialIcon";
 
@@ -43,11 +43,7 @@ const inputStyle = "flex-1 rounded-md bg-black text-white px-6 py-2  outline-non
       <div className="flex gap-10 pt-10">
         <div className="w-1/3 bg-stone-950/50 p-10 rounded-lg group">
           <div className="overflow-hidden rounded-lg">
-            <img
-              src={contact1}
-              alt="contact image"
-              className="rounded-md group-hover:scale-110 duration-500"
-            />
+            '1'
           </div>
           <div className="py-5 flex flex-col gap-2">
             <p className="text-lg text-white font-bold">Martin</p>
