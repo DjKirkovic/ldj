06a4dba176a4fc857d0a883080a1133395e8e1f9
Martin Kirk <kirkovic@gmail.com>
2026-01-08 15:36:21 +0100

    page titles 3

--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Title = ({ title }) => {
-  return <h5 className="text-6xl font-bold text-titleColour">{title}</h5>;
+  return <h5 className="text-3xl font-bold text-titleColour">{title}</h5>;
 };
 
 export default Title;
