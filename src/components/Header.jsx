e7bfbf10f9fb77aeb18cb44e8fc93d5d3e34ea85
Martin Kirk <kirkovic@gmail.com>
2026-02-20 11:15:41 +0100

    serv tab upd 9

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../assets/logonew.png";
 import Button from "./ui/Button";
 import { useLocation } from "react-router-dom";
 
