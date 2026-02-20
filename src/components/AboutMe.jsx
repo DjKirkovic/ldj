644d783e015d772a0401b39c79c5fc957eaecd45
Martin Kirk <kirkovic@gmail.com>
2026-02-20 12:37:18 +0100

    serv tab upd 14

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -31,10 +31,10 @@ const AboutMe = () => {
           </p>
 
           <p><b>The Founder</b><br />
-            Meet Martin. Based in Denmark, he's not your typical Dane. He is half Egyptian, spent half his life in the UK, 'represents' three further nationalities via marriage, and has travelled across four continents. 
+            Meet Martin. Based in Denmark, he's not your typical Dane. He is half Egyptian, spent a decade in the UK, 'represents' three further nationalities via marriage, and has travelled across four continents. 
           </p>
           <p><b>Why/Why Not...?</b><br />
-            Feedback, good as bad, is always welcome! Drop us a line via the Contact page to help us help you <i>better</i>! 
+            Feedback, positive as negative, is always welcome! Head for the Contact page to help us help you <i>better</i>! 
           </p>
         </div>
       </div>
