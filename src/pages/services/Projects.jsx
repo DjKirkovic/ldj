2dfd94267325ba3af7627003ada12756b2de5797
Martin Kirk <kirkovic@gmail.com>
2026-03-28 14:03:35 +0100

    proj 1

--- src/pages/services/Projects.jsx
+++ src/pages/services/Projects.jsx
@@ -13,8 +13,26 @@ const Projects = () => {
         </div>
    </div>
 
-    <p><b>Subheader</b><br />
-        abc.<br />def 
+    <p><b>KISS</b><br />
+        KISS {'('}= Keep It Simple, Stupid{')'} <br /><br />This section is the future home of many things interesting, great or plain ingenius. 
+<br /><br />
+ </p>
+    <p><b>Web publishing</b><br />
+        Writing for the web is impossibly hard. Trust me, I know the feeling!<br /><br />So what insights can <i>I</i> bring you here? First, here's my resume for web writing:<br />
+        <ul class="list-disc mb-8 ml-4">  
+              <li> Corporate websites </li>
+              <li> Press releases on intl. portals like EurekAlert </li>
+              <li> News articles </li>
+              <li> Magazines </li>
+              <li> Brochures </li>
+               </ul>
+        
+      Next, here's my wisdom:<br />
+      Find your scene: What's your message, why, and is the web at all the right place for it?
+      <br /><br />
+      </p>
+    <p><b>Web research</b><br />
+        The internet is packed 
     </p>
    
     </Container>
