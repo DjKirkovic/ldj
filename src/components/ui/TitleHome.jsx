1954d58cbbb01fb2ae579a670e6b177c05e81acb
Martin Kirk <kirkovic@gmail.com>
2026-05-07 16:34:04 +0200

    pg 14

--- src/components/ui/TitleHome.jsx
+++ src/components/ui/TitleHome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TitleHome = ({ title }) => {
-  return <h5 className="text-3xl font-bold text-blue-500">{title}</h5>;
+  return <h5 className="text-3xl font-bold text-white">{title}</h5>;
 };
 
 export default TitleHome;
