d655e279c3f8fcf840a555690cf9db69a1add990
Martin Kirk <kirkovic@gmail.com>
2026-05-07 17:47:09 +0200

    pg 15

--- src/components/ui/SocialIcon.jsx
+++ src/components/ui/SocialIcon.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa6";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa6";
 
 const portfolioIconStyle = "bg-stone-800 w-10 h-10 inline-flex text-white text-base justify-center items-center rounded-md hover:text-designColor duration-500 hover:-translate-y-2 hover:bg-black shadow-md shadow-gray-200/10 hover:shadow-designColor/10;"
 const SocialIcon = () => {
   return (
     <div className="flex items-center gap-5">
+      
+      <a title="Instagram" href="https://www.instagram.com/foto247.dk/" target="_blank" rel="noopener noreferrer" className={portfolioIconStyle}>
+        <FaInstagram />
+      </a> 
       <a title="LinkedIn" href="https://www.linkedin.com/in/martinkirk/" target="_blank" rel="noopener noreferrer" className={portfolioIconStyle}>
         <FaLinkedin />
       </a>
