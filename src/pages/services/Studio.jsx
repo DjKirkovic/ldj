edd8550f2c7eb17f251e2238f07d48bfd3855341
Martin Kirk <kirkovic@gmail.com>
2026-05-08 14:00:14 +0200

    page expansions

--- src/pages/services/Studio.jsx
+++ src/pages/services/Studio.jsx
@@ -23,7 +23,47 @@ const Studio = () => {
                />
      </div>
 
-<p><br /><b>Info på vej...<br /></b></p>
+<p><br /><b>SOS krisehjælp er guld værd...<br /></b>
+Hvis du står med et SOS-behov, så læs videre!<br />
+Bemærk at assistance/opgaver som ikke er af SOS kaliber &#40;hverken tidspressende eller tilstrækkeligt vigtige/alvorlige&#41; behandles særskilt.<br /><br />
+Kontakt os på bedst mulige måde i forhold til din forespørgsels prioritet:<br />
+              <ol class="list-disc mb-8 ml-4">  
+                <li> 
+                  Akut behov - du behøver assistance NU!
+                </li>
+                <li> 
+                  Haster - du skal bruge hjælp snarest muligt
+                </li>
+                <li> 
+                  Vigtigt assistancebehov - den hjælp du søger har betydelig påvirkning eller følger
+                </li>
+                </ol>
+
+De 3 beredskabsniveauer &#40;1=højeste og 3=laveste&#41; kræver alle typisk telefonisk henvendelse.<br />
+Alternativt kan webformularen på 'Kontakt' siden bruges - skriv da prioritetsnummeret i emnefeltet.<br />
+Sendes en besked, gives ingen garanti for hurtigt svar/assistance, især udenfor normal arbejdstid.<br /><br />
+
+Eksempler på SOS assistance/opgaver er, at du:
+
+<ol class="list-disc mb-8 ml-4">  
+                <li> 
+                  Behøver en fotograf som følge af fx underkendt behov &#40;din opgave kræver yderligere fotografer&#41;, udeblivelse eller sygdom
+                </li>
+                <li> 
+                  Mangler hjælp/rådgivning eller udstyr
+                </li>
+                <li> 
+                  Har brug for assistance ift. vigtig levering af billeder
+                </li>
+                <li> 
+                  Er på vej på en tur/rejse, hvor den hjælp du behøver har vigtig impakt herpå
+                </li>
+                <li> 
+                  Skal gennemføre en ordre eller et køb, som er tidspressende/korttidsbestemt
+                </li>
+                </ol>
+Hvis du behøver hjælp/vejledning, kontakt os da endelig, hvormed vi kan guide dig sikkert videre/i mål.
+</p>
 
     </Container>
   );
