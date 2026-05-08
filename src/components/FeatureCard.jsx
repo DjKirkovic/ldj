8165b150d2f26bd07b807473f590d76fca7690de
Martin Kirk <kirkovic@gmail.com>
2026-05-08 09:11:34 +0200

    page expansions

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { IoMdArrowRoundForward } from "react-icons/io";
 
 const FeatureCard = ({ item }) => {
-const iconstyle = "text-2xl text-green-500";//designColor
+//const iconstyle = "text-2xl text-green-500";//designColor
   return (
     <a href={item.link}>
     <div className="w-full p-10 bg-black flex flex-col gap-5 rounded-lg shadow-lg shadow-gray-500/10">
-      <span className={iconstyle}>{item.img}</span>
+      <img src={item.img} alt="logo" className="w-24" />
       <p className="text-lg font-bold">{item.title}</p>
       <p className="text-base font-medium">{item.description}</p>
     </div>
