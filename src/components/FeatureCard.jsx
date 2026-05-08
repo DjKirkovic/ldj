49a05cf94369aa4a424d5ac98d91601480d56a99
Martin Kirk <kirkovic@gmail.com>
2026-05-08 17:15:47 +0200

    page expansions

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -19,7 +19,7 @@ const FeatureCard = ({ item }) => {
 //const iconstyle = "text-2xl text-green-500";//designColor
   return (
     <a href={item.link}>
-    <div className="w-full p-10 bg-black flex flex-col gap-5 rounded-lg shadow-lg shadow-gray-500/10">
+    <div className="w-full p-10 bg-black flex flex-col gap-2 rounded-lg shadow-lg shadow-gray-500/10">
       <img src={pics[(item.id)-1]} />
       <p className="text-lg font-bold">{item.title}</p>
       <p className="text-base font-medium">{item.description}</p>
