eda6238a9a6b8d3d37b05c6b47df5812e1117642
Martin Kirk <kirkovic@gmail.com>
2026-05-09 07:49:20 +0200

    page expansions

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -27,7 +27,7 @@ const AboutUs = () => {
         <div className="w-[61%] flex flex-col gap-7 text-black">
           
           <p><b>Historie</b><br />
-            Foto247 blev stiftet i 2026, men har rødder tilbage til 2003. Da startede stifteren en medieproduktionsuddannelse.
+            Foto247 blev etableret i Danmark i 2014, med rødder tilbage til 2003. Da startede stifteren sin uddannelse.
             Firmaets filosofi er at tilbyde fantastiske foto-services til gode priser - og fleksibelt at tage hånd om kundeønsker.<br /><br />
             Hvadend du er firmarepræsentant eller privatperson, er det vores fornøjelse at realisere dit næste foto-projekt. 
           </p>
