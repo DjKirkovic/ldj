d368d8c0bb961054906ef4d0e655aff6a878cbd8
Martin Kirk <kirkovic@gmail.com>
2026-05-13 16:00:33 +0200

    page expansions

--- src/constant/data.jsx
+++ src/constant/data.jsx
@@ -93,7 +93,7 @@ export const featuresData = [
     //icon: <IoBookOutline />,
     title: "Teknik / Udstyr",
     description:
-      "Hjælp med udstyr, system/funktion/proces, køb/salg,...",
+      "Udstyrshjælp, system/proces, funktioner, køb/salg,...",
     //link: onClick={()=>console.log(item)}
     link: "#/teknik",
   },
