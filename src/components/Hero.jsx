f2bb87198cd5421e1055361af1f809e0055108ba
Martin Kirk <kirkovic@gmail.com>
2026-05-13 16:45:37 +0200

    page expansions

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
 <h4>Velkommen!</h4>
 
           <div className="text-base text-lightText pt-4 font-medium">
-            <p>Vi tilbyder professionelle fotos via:</p>
+            <p>Vi tilbyder professionelle fotos:</p>
               <ul class="list-disc mb-4 ml-4">  
                 <li><Link to={"studie"} class="text-blue-500 hover:text-green-500">Portræt</Link></li>
                 <li><Link to={"event"} class="text-blue-500 hover:text-green-500">Events</Link></li>
