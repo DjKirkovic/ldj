ba801ebc28baa01b7de21a0827ddc4ee6185a3af
Martin Kirk <kirkovic@gmail.com>
2026-05-13 15:29:05 +0200

    page expansions

--- src/constant/data.jsx
+++ src/constant/data.jsx
@@ -52,7 +52,7 @@ export const featuresData = [
     //icon: <AiOutlineMenu />,
     title: "Mere/Andet",
     description:
-      "Andre genrer: PR, mode, stock fotos, research, mad, dokumentar...",
+      "Andre genrer: PR, mode, stock fotos, research, mad, kultur...",
     link: "#/divfoto",
   },
   {
