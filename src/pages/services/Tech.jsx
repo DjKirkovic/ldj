eeb5894392797f680cf79024f4d5f9253ea93bfc
Martin Kirk <kirkovic@gmail.com>
2026-08-31 12:51:55 +0200

    udstyr

--- src/pages/services/Tech.jsx
+++ src/pages/services/Tech.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import React from "react";
 import Container from "../../components/Container";
 import Subtitle from "../../components/ui/Subtitle";
@@ -18,12 +19,57 @@ const Tech = () => {
 <div className="overflow-hidden rounded-lg">
                <img
                  src={te}
-                 alt="Studie"
+                 alt="Udstyr"
                  className="rounded-md group-hover:scale-110 duration-500"
                />
      </div>
 
-<p><br /><b>Info på vej...<br /></b></p>
+<p><br /><b>Søger du info og råd om fotoudstyr, -teknik og anvendelse?<br /></b>
+Udstyr er et enormt bredt emne, og selvom vi aldrig kan komme 'helt rundt', kan vi hjælpe dig godt på vej.
+
+Eksempler på tilbudt udstyrshjælp:
+
+<ol class="list-disc mb-8 ml-4">  
+                <li> 
+                  Kamera-/objektiv-/systemvalg
+                </li>
+                <li> 
+                  Fotoanvendelser/områder/formål
+                </li>
+                <li> 
+                  Teknikker/metoder/begreber i fotografering
+                </li>
+                <li> 
+                  Billedbehandling/redigering
+                </li>
+                <li> 
+                  Lyssætning
+                </li>
+                <li> 
+                  Videooptagelse
+                </li>
+                <li> 
+                  Køb/salg
+                </li>
+                </ol>
+
+Vi tilbyder flere leveringsformer:
+<ol class="list-disc mb-8 ml-4">
+                <li> 
+                  Personligt fremmøde
+                </li>
+                <li> 
+                  Telefonisk assistance
+                </li>
+                <li> 
+                  Skriftlig leverance
+                </li>
+                </ol>
+
+Kontakt os og hør nærmere ift. hvad vi kan tilbyde dig eller dit firma!<br />
+
+Haster dit behov? Så klik på <Link to={"kontakt"} class="text-blue-500 hover:text-green-500">kontakt</Link> og ring/skriv!
+</p>
 
     </Container>
   );
