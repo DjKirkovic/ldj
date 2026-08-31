f5ac0e7e272cc0ae1cff5cec976bd0b00e424c2a
Martin Kirk <kirkovic@gmail.com>
2026-08-31 17:15:26 +0200

    kontakt

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,8 +49,8 @@ const Hero = () => {
               </ul>
               
               <p class="mb-8">
-                Websiden er under ombygning &#40;august 2026&#41;, så hold <br />
-                øje med nyt på servicesider, om/kontakt og billedgalleri!
+                Websiden er under ombygning &#40;september 2026&#41;, så hold <br />
+                øje med nyt på siderne om/kontakt samt billedgalleri!
               </p>
 
               <p class="mb-8">
