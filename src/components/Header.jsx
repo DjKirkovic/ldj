65d1ba082e3fff92f4022e048fae9d4bd7c20fae
Martin Kirk <kirkovic@gmail.com>
2026-08-31 15:24:42 +0200

    nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
   const navigationArray = [
     { title: "Home", link: "/" },
     { title: "Services", link: "/services" },
-    { title: location.pathname.split('/')[2] === 'undefined' ? "" : pathname.split('/')[2], link: location.pathname },
+    { title: pathname.split('/')[2] === 'undefined' ? "" : pathname.split('/')[2], link: pathname },
     { title: "Fotos", link: "/fotos" },
     { title: "Om", link: "/om" },
     { title: "Kontakt", link: "/kontakt" },
