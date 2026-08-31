276f99dda82077cf764adedfcf45905a4f3f0d0c
Martin Kirk <kirkovic@gmail.com>
2026-08-31 11:11:32 +0200

    råd

--- src/pages/services/Advice.jsx
+++ src/pages/services/Advice.jsx
@@ -18,12 +18,51 @@ const Advice = () => {
 <div className="overflow-hidden rounded-lg">
                <img
                  src={ed}
-                 alt="Studie"
+                 alt="Rådgivning"
                  className="rounded-md group-hover:scale-110 duration-500"
                />
      </div>
 
-<p><br /><b>Info på vej...<br /></b></p>
+<p><br /><b>Savner du indsigt, rådgivning eller har undervisningsbehov?<br /></b>
+Vi hjælper med en bred vifte af services, så du kan få fotorådgivning, -viden eller -undervisning til dit publikum.<br />
+
+Eksempler på fotoområder:
+
+<ol class="list-disc mb-8 ml-4">  
+                <li> 
+                  Udstyr
+                </li>
+                <li> 
+                  Anvendelse 
+                </li>
+                <li> 
+                  Rådgivning
+                </li>
+                <li> 
+                  Køb/salg
+                </li>
+                <li> 
+                  Undervisning &#40;fx foredrag, tutoring/1:1 assistance, skriftligt materiale&#41;
+                </li>
+                </ol>
+
+Vi tilbyder flere leveringsformer:
+<ol class="list-disc mb-8 ml-4">
+                <li> 
+                  Personligt fremmøde
+                </li>
+                <li> 
+                  Telefonisk assistance
+                </li>
+                <li> 
+                  Skriftlig leverance
+                </li>
+                </ol>
+
+Med bred erfaring og fleksible løsninger, styrer vi dig/jer sikkert i mål med dine/jeres ønsker.<br />
+
+Haster dit event? Så klik på <Link to={"kontakt"} class="text-blue-500 hover:text-green-500">kontakt</Link> og ring/skriv!
+</p>
 
     </Container>
   );
