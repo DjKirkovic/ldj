a8e442c93dd43c957793b4158a321f3a988fc776
Martin Kirk <kirkovic@gmail.com>
2026-08-31 09:40:41 +0200

    produkt

--- src/pages/services/Stuff.jsx
+++ src/pages/services/Stuff.jsx
@@ -18,13 +18,53 @@ const Stuff = () => {
 <div className="overflow-hidden rounded-lg">
                <img
                  src={pr}
-                 alt="Studie"
+                 alt="Produkt"
                  className="rounded-md group-hover:scale-110 duration-500"
                />
      </div>
 
-<p><br /><b>Info på vej...<br /></b></p>
+<p><br /><b>Fremvis og sælg dit produkt bedre!<br /></b>
+Du har det perfekte produkt, men mangler retfærdiggørende billeder?<br />
+Udnyt potentialet ved at lade os hjælpe!<br /><br />
 
+Eksempler på produkter:
+
+<ol class="list-disc mb-8 ml-4">  
+                <li> 
+                  Smykker
+                </li>
+                <li> 
+                  Tøj/Mode 
+                </li>
+                <li> 
+                  Boligudstyr
+                </li>
+                <li> 
+                  Elektronik
+                </li>
+                <li> 
+                  Skønhed/Helse
+                </li>
+                <li> 
+                  Kreativt/Dekorationer
+                </li>
+                </ol>
+
+Vi tilbyder:
+<ol class="list-disc mb-8 ml-4">
+                <li> 
+                  Studie-baserede 'product shoots'
+                </li>
+                <li> 
+                  On-location &#40;dit produkt på din foretrukne lokation&#41;
+                </li>
+                <li> 
+                  In situ - dit produkt i brugssituationer &#40;i fremstilling, på udstilling, eller i brug&#41;
+                </li>
+                </ol>
+
+Dine ønsker er vores prioritet, så kontakt os for en snak om muligheder.<br />
+</p>
     </Container>
   );
 };
