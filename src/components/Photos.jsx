b69d619153be574fd02a9b6be549d08e00bf7efc
Martin Kirk <kirkovic@gmail.com>
2026-09-04 10:53:25 +0200

    gallery

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -78,7 +78,7 @@ const Photos = () => {
       </div>
 
 
-      <div className="grid grid-cols-3 gap-2 pt-10" style={{ display: toggle ? 'block' : 'none' }}> 
+      <div className="grid grid-cols-3 gap-2 pt-10" style={{ display: toggle ? 'grid grid-cols-3 gap-2 pt-10' : 'none' }}> 
       {
                 Object.values(images).map((image, index) => (
                   <img key={index} src={image.default} alt={'Image ${index + 1}'} title={''+index+imgTitles[index]} onClick={(index) => handleClick(index)} />
