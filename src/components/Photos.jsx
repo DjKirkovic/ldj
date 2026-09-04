2be3a17a540e4033d0fd4d40bbdf6a7cb1c7444d
Martin Kirk <kirkovic@gmail.com>
2026-09-04 12:13:39 +0200

    gallery

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -59,11 +59,11 @@ const Photos = () => {
   
   const index = 0; // Initialize index to 0
 
-  const handleClick = useCallback((index) => {
+  const handleClick = useCallback((index, key) => {
     setToggle(!toggle);
     console.log('Image clicked:', index);
     index = index; // Update the index with the clicked image's index
-    console.log(index.key);
+    console.log(key);
   }, [toggle]);
    
 
@@ -82,7 +82,7 @@ const Photos = () => {
       <div className="grid grid-cols-3 gap-2 pt-10" style={{ display: toggle ? 'grid' : 'none' }}> 
       {
                 Object.values(images).map((image, index) => (
-                  <img key={index} src={image.default} alt={'Image ${index + 1}'} title={''+index+imgTitles[index]} onClick={(index) => handleClick(index)} />
+                  <img key={index} src={image.default} alt={'Image ${index + 1}'} title={''+index+imgTitles[index]} onClick={(index, key) => handleClick(index, key)} />
                 ))
       }
 
