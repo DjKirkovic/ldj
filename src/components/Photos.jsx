e8196cb1a114abd9d7614b476d6d79b51c196fad
Martin Kirk <kirkovic@gmail.com>
2026-09-05 13:44:27 +0200

    gallery

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -66,7 +66,7 @@ const [index, setIndex] = useState(-1);
         captionsTitleContainer: { 
           top: "85%",
           right: "-70%",
-          bottom: "85%",
+          bottom: "95%",
           
           //left: "50%",
           textAlign: "center",
