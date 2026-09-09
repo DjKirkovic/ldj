c2111f9724cbda6eb11bc77ff006ab860302403c
Martin Kirk <kirkovic@gmail.com>
2026-09-09 14:00:47 +0200

    gallery

--- src/constant/slides.jsx
+++ src/constant/slides.jsx
@@ -33,7 +33,7 @@ export const slides = [
             description: 'Solnedgang over byens vartegn',
             cat: '1',
           },
-          { src: iArc[`${prefix}01/04.jpg`].default,
+          { src: iArc[`${prefix}01/041.jpg`].default,
             title: 'Burj Khalifa, Dubai, UAE',
             description: 'Verdens højeste bygning - på 829m!',
             cat: '1',
