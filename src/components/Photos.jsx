80871fe76b92f125566b117d56f526def8e1040b
Martin Kirk <kirkovic@gmail.com>
2026-09-09 11:54:21 +0200

    gallery

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -23,34 +23,6 @@ const Photos = () => {
   const [selectedCats, setSelectedCats] = useState([]);
   
   const [fslides, setSlides] = useState([]);
-  
-
-
-/*
-
-
-
- const handleAddDel = (indexI) => {
-  console.log("bb");
- 
-  //setSlides((prev) => {
-  setSlides((preva) => {
-//  for (const origElement of slides) { //for ea (img) object for this category's index (indexI) in the slides array
-    //for (const destElement of prev) { //for ea (img) object in the State slides array
-    preva.includes (slides[0]) ? 
- //   prev.includes (origElement)  
- // ? prev.filter((item) => item.cat !== indexI) // Remove if already checked
-   console.log('dd') // Remove if already checked
-  // : [...prev, slides.origElement]                     // Add if unchecked
-  //: [...prev, slides[0]]                     // Add if unchecked
-  : console.log('ee') // Remove if already checked
-  //}
-}
-);
-
-};
-
-*/
 
   const handleToggle = (cat) => {
     setSelectedCats((prev) =>
@@ -59,93 +31,16 @@ const Photos = () => {
         : [cat]                     // Add if unchecked
       );
       
-      //Now that user has checked/unchecked a cat, update slides shown.
-      //1: Find out which cat was clicked
       const changedCat = cat;
-      //console.log('cat: '+changedCat); //as cat string
       const indexI = CATS.indexOf(changedCat)+1; //assign single int (1-10) representing 10 cats
-//      console.log(indexI);
       const newSlides = indexI == 11 ? //if 11 (1st/if statement), show all. Otherwise, show chosen cat's thumbnail images
         slides
-        //('sgl')
         :
-        //('dual')
         slides.filter(slide => slide.cat === ""+indexI)
-       setSlides(newSlides);
-        //console.log(newSlides);
-     
-     // const catNumbers = indexI.toString().length == 1 ? '0'+indexI : indexI; // the two-digit no. for cat clicked
-     
-     
-      //console.log('complete dig: '+catNumbers);
-      /*  
-       console.log('singl: '+indexI.toString().length)
-       :
-        console.log('dual?: '+indexI.toString().length)
-      */
+        setSlides(newSlides);
 
-        //single int
-
-      //console.log('IndexLocation?: '+indexLoc); //as integer
-      
-
-      //2: Find out if category was added or removed
- //     const catWasAdded = selectedCats.includes(changedCat) ? false : true; //if found in array, cat was already there and needs removing. Else, needs adding
-      //console.log('was...: '+catWasAdded);
-
-      //3: if added, add cat thumbnail images to array. Otherwise, remove them:      
-      
-      //if first added, stock up state slides from import slides to facilitate filtering
-      
-      //setSlides(selectedCats.length == 0 ? slides : slides);
-      //console.log(selectedCats.length);
-      
-      //selectedCats.length == "0" & selectedCats.includes(changedCat)? setSlides(slides) : null
-      //console.log(fslides);
-//   const newSlides = catWasAdded ?
-        //slides.filter(slide => slide.cat === ""+indexI)
-        //fslides.append(slides.filter(slide => slide.cat === ""+indexI))
-//    slides.filter(slide => slide.cat === ""+indexI)
-
-
-//setSlides(prev => ({ fslides: [...fslides, slides.filter(slide => slide.cat === ""+indexI)] }))
-
-
-
-//console.log("aa");
-//handleAddDel(ind);
-//console.log("cc");
-
-//setSlides([...fslides, slides.filter(slide => slide.cat === ""+indexI)])
- //   :
-//    slides.filter(slide => slide.cat !== ""+indexI)
-        
- //       setSlides([...fslides, slides.filter(slide => slide.cat === ""+indexI)]);
-
-  /*      slides.filter(slide => slide.src !== "/src/assets/portfolioImages/01/"+*+".jpg")
-      //console.log('added')
-      
-      //const newSlides = slides.filter(slide => slide.src !== "/src/assets/portfolioImages/01/01.jpg");
-      
-        : 
-        slides.filter(slide => slide.src !== "/src/assets/portfolioImages/01/02.jpg")
-      
-
-      setSlides(newSlides);
-      //console.log('removed')
-    */  
-/*
-      const newSlides = slides.filter(slide => slide.src !== "/src/assets/portfolioImages/01/01.jpg");
-      
-      setSlides(newSlides);
-  */
-    //  console.log(newSlides[1]);
-    //  console.log(fslides[1]);
-      //console.log(this.state.fslides.length());
     };
-   
-      
-    
+       
     return (
       <Container className="border-black">
       <div className="flex flex-col items-center">
@@ -168,7 +63,6 @@ const Photos = () => {
             </label>
           ))}
           <p>Antal billeder vist: {<span style={{color: "black"}}>{fslides.length}</span>}</p>
-          {/* <p>Valgt: {selectedCats.join(', ') || 'Ingen'}</p> */}
         </div>
 
         <Pics data={fslides} onClick={(currentIndex) => setIndex(currentIndex)} />
@@ -176,10 +70,8 @@ const Photos = () => {
         <Lightbox
         plugins={[Counter, Captions, Fullscreen, Zoom, Thumbnails, Slideshow]}
         //title={{ container: {style: {top: 100, bottom: 100}}}}
-        
         styles={{ 
-          //maxWidth: 600, margin: "20px auto", maxChars: 100,
-          
+          //maxWidth: 600, margin: "20px auto", maxChars: 100, 
           captionsTitle: { 
             fontSize: "0.7rem", //color: "blue", overflow: "visible", marginRight, marginLeft: "2%",
             textAlign: "center",
@@ -214,8 +106,7 @@ const Photos = () => {
           open={index >= 0}
           close={() => setIndex(-1)}
           slides={fslides}
-          />
-          
+          />   
       </div>  
     </Container>
   );
@@ -224,13 +115,5 @@ const Photos = () => {
 export default Photos;
 
 /*
+Lightbox credit:
 MIT License , Copyright (c) 2022 Igor Danchenko */
-
-/*
-const portfolioImages = import.meta.glob('../assets/portfolioImages/*.{png,jpg,jpeg,svg}', {
-   eager: true,
-   query: '?url',
-   import: 'default',
-});
-*/
-//const images = import.meta.glob('../assets/portfolioImages/*.{png,jpg,jpeg,svg}', { eager: true }); // Dynamically import all (image) files in the portfolioimages folder:
