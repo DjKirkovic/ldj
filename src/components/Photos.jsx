66dff2c7465d81a50abacd46cd7d391998985dc6
Martin Kirk <kirkovic@gmail.com>
2026-09-10 11:12:22 +0200

    gallery

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -68,7 +68,7 @@ const modArray = newSlides.map(o => ({ ...o, width: 2100 }));
       <div> 
       
       {/* <button type="button" onClick={() => setOpen(true)}>Open Lightbox</button> */}
-        <div style={{ display: 'flex', color: 'green', width: '100vw', alignItems: "center", justifyContent: "center" }}> 
+        <div style={{ display: 'flex', color: 'green', width: '100%', alignItems: "center", justifyContent: "center" }}> 
          {/*  <p>Vælg kategori(er):</p> */}
           {CATS.map((cat) => (
             <label key={cat} style={{ display: 'inline', padding: '0px', borderLeft: '0px', letterSpacing: '0px', fontWeight: cat === "Alle" ? "bold" : "unset"}}>
@@ -82,8 +82,6 @@ const modArray = newSlides.map(o => ({ ...o, width: 2100 }));
           <div style={{display:'flex', color: 'black', label: fslides.length == 0 ? "abc def" : "", visibility: fslides.length == 0 ? "hidden" : "visible", alignItems: "center", justifyContent: "center"}}>Antal billeder vist: {<span style={{color: "black", paddingLeft: '5px',}}>{" "+fslides.length.toString()}</span>}</div>
           <div style={{display:'flex', color: "white", width: fslides.length == 0 ? "100%" : "0px", height: fslides.length == 0 ? "100px" : "0px", backgroundColor:"grey", label: fslides.length == 0 ? "abc def" : "", visibility: fslides.length == 0 ? "visible" : "hidden", alignItems: "center", justifyContent: "center"}}>Vælg en kategori</div>
           <div style={{ display: 'flex', width: '100vw'}}>
-
-          <div style={{display:'flex', color: "white", width: fslides.length == 0 ? "100%" : "0px", height: fslides.length == 0 ? "100px" : "0px", backgroundColor:"green", label: fslides.length == 0 ? "abc def" : "", visibility: fslides.length == 0 ? "visible" : "hidden", alignItems: "center", justifyContent: "center"}}></div>
           </div>
         <Pics data={fslides} onClick={(currentIndex) => setIndex(currentIndex)} />
 <div style={{ display: 'block'}}>
