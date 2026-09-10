cd2e7614771392ee4b197cc6cd15e9eb3aea7789
Martin Kirk <kirkovic@gmail.com>
2026-09-10 12:48:15 +0200

    gallery

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -73,8 +73,8 @@ const modArray = newSlides.map(o => ({ ...o, width: 2100 }));
          
          {/*  <p>Vælg kategori(er):</p> */}
           {CATS.map((cat) => (
-            <label key={cat} style={{ display: 'flex-inline', justifyContent: "center", padding: '0px', borderLeft: '0px', letterSpacing: '0px', fontWeight: cat === "Alle" ? "bold" : "unset"}}>
-              <input type="radio" checked={selectedCats.includes(cat)} style={{display: 'flex-inline', marginRight: '0px'}} onChange={() => handleToggle(cat)}/>
+            <label key={cat} style={{ display: 'inline', justifyContent: "center", padding: '0px', borderLeft: '0px', letterSpacing: '0px', fontWeight: cat === "Alle" ? "bold" : "unset"}}>
+              <input type="radio" checked={selectedCats.includes(cat)} style={{display: 'inline', marginRight: '0px'}} onChange={() => handleToggle(cat)}/>
               {cat}
             </label>
           ))}
@@ -85,7 +85,7 @@ const modArray = newSlides.map(o => ({ ...o, width: 2100 }));
           <div style={{display:'block', color: "white", width: fslides.length == 0 ? "20%" : "0px", height: fslides.length == 0 ? "100px" : "0px", backgroundColor:"grey", label: fslides.length == 0 ? "abc def" : "", visibility: fslides.length == 0 ? "visible" : "hidden", alignItems: "center", justifyContent: "center"}}>Vælg en kategori</div>
           <div style={{ display: 'block', width: '100vw'}}>
 
-          <div style={{display:'block', color: "white", width: fslides.length == 0 ? "100vw" : "0px", height: fslides.length == 0 ? "100px" : "0px", backgroundColor:"red", label: fslides.length == 0 ? "abc def" : "", visibility: fslides.length == 0 ? "visible" : "hidden", alignItems: "center", justifyContent: "center"}}></div>
+          <div style={{display:'block', color: "white", width: fslides.length == 0 ? "100vw" : "0px", height: fslides.length == 0 ? "100px" : "0px", backgroundColor:"blue", label: fslides.length == 0 ? "abc def" : "", visibility: fslides.length == 0 ? "visible" : "hidden", alignItems: "center", justifyContent: "center"}}></div>
           </div>
         <Pics data={fslides} onClick={(currentIndex) => setIndex(currentIndex)} />
 <div style={{ display: 'flex', width: '100px'}}>
