cf0a7fb25f8d981d2eabcdc05fd3064d0235b6e8
Martin Kirk <kirkovic@gmail.com>
2026-09-10 11:00:35 +0200

    gallery

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -81,7 +81,7 @@ const modArray = newSlides.map(o => ({ ...o, width: 2100 }));
         </div>
           <div style={{display:'flex', color: 'black', label: fslides.length == 0 ? "abc def" : "", visibility: fslides.length == 0 ? "hidden" : "visible", alignItems: "center", justifyContent: "center"}}>Antal billeder vist: {<span style={{color: "black", paddingLeft: '5px',}}>{" "+fslides.length.toString()}</span>}</div>
           <div style={{display:'flex', color: "white", width: fslides.length == 0 ? "100%" : "0px", height: fslides.length == 0 ? "100px" : "0px", backgroundColor:"grey", label: fslides.length == 0 ? "abc def" : "", visibility: fslides.length == 0 ? "visible" : "hidden", alignItems: "center", justifyContent: "center"}}>Vælg en kategori</div>
-          
+          <div style={{ display: 'block', width: '100vw'}}></div>
         <Pics data={fslides} onClick={(currentIndex) => setIndex(currentIndex)} />
 <div style={{ display: 'block'}}>
         <Lightbox
