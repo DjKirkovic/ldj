6d1ceeeadc392d147bbd808ba725322bcbfaa457
Martin Kirk <kirkovic@gmail.com>
2026-09-10 10:56:57 +0200

    gallery

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -83,7 +83,7 @@ const modArray = newSlides.map(o => ({ ...o, width: 2100 }));
           <div style={{display:'flex', color: "white", width: fslides.length == 0 ? "100%" : "0px", height: fslides.length == 0 ? "100px" : "0px", backgroundColor:"grey", label: fslides.length == 0 ? "abc def" : "", visibility: fslides.length == 0 ? "visible" : "hidden", alignItems: "center", justifyContent: "center"}}>Vælg en kategori</div>
           
         <Pics data={fslides} onClick={(currentIndex) => setIndex(currentIndex)} />
-<div style={{ display: 'flex'}}>
+<div style={{ display: 'block'}}>
         <Lightbox
         plugins={[Counter, Captions, Fullscreen, Zoom, Thumbnails, Slideshow]}
         //title={{ container: {style: {top: 100, bottom: 100}}}}
